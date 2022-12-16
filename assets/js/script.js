dfe765da6dbb1b44d5242a6a479a1d6a7890d889
Kaushal Subba <kaushal.subba@gmail.com>
2022-12-16 10:34:41 +0545

    Updated with new sections and scroll animation experiments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,46 @@
-// Hero Section Mouse Interaction
-var heroSection=document.querySelector("section#hero");
-heroSection.addEventListener('mousemove',e=>{
-    // Get the root element
-    var r = document.querySelector(':root');
+// Get the root element
+var r = document.querySelector(':root');
+var sprinkles=document.querySelectorAll("#hero .sprinkles span");
 
-    r.style.setProperty('--heroPositionX', e.clientX);
-    r.style.setProperty('--heroPositionY', e.clientY);
+// Hero Section Mouse Interaction
+// var body=document.querySelector("body");
+var reduceBy=2;
+var clientY=20;
+var clientX=20;
+var reduce=false;
+window.addEventListener('scroll',e=>{
+    if(r){        
+        if(clientX>=250) reduce=true;
+        if(clientX<=100) reduce=false;
+
+        console.log(reduce,clientX);
+
+        if(reduce){
+            clientY-=reduceBy;
+            clientX-=reduceBy;
+        } else {
+            clientY+=reduceBy;
+            clientX+=reduceBy;
+        }
+
+        // clientY=e.clientX;
+        // clientY=e.clientY;
+
+
+
+        r.style.setProperty('--heroPositionX', clientX);
+        r.style.setProperty('--heroPositionY', clientY);
+
+        // if(sprinkles){
+        //     // console.log(sprinkles);
+        //     for(i=0;i<=sprinkles.length;i++){
+        //         if(sprinkles[i]){
+        //             currentLeft=sprinkles[i].offsetLeft;
+        //             console.log(currentLeft,e.clientX/100);
+        //             sprinkles[i].style.left=currentLeft-e.clientX/100+'px';
+        //         }
+        //     }
+
+        // }
+    }
 })
